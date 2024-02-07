117be51f23de9a78be58ccf993aaea61caffd5c2
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2024-02-07 10:43:54 -0800

    feat(flake): added formatter

--- flake.nix
+++ flake.nix
@@ -3,29 +3,41 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = inputs@{ self, flake-parts, nixpkgs, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    nixpkgs,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       flake = {
-        lib = import ./lib { inherit inputs; };
+        lib = import ./lib {inherit inputs;};
       };
 
-      systems = [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
+      systems = ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"];
 
-      perSystem = { config, self', inputs', pkgs, system, ... }:
-        let
-          inherit (pkgs) just mkShell;
-        in
-        {
-          devShells = {
-            default = mkShell {
-              buildInputs = [ just ];
-            };
-          };
-
-          packages = {
-            default = self.lib.mkVimPlugin { inherit system; };
-            neovim = self.lib.mkNeovim { inherit system; };
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: let
+        inherit (pkgs) just mkShell;
+      in {
+        devShells = {
+          default = mkShell {
+            buildInputs = [just];
           };
         };
+
+        formatter = pkgs.alejandra;
+
+        packages = {
+          default = self.lib.mkVimPlugin {inherit system;};
+          neovim = self.lib.mkNeovim {inherit system;};
+        };
+      };
     };
 }
