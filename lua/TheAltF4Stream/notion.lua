2c9b398cbc6b6247bd07fbf93c6873104cb6ed0e
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2025-12-11 00:59:00 -0800

    feat(notion): updated package and added keymaps

--- lua/TheAltF4Stream/notion.lua
+++ lua/TheAltF4Stream/notion.lua
@@ -1,6 +1,11 @@
 local notion = require 'notion'
 
 local function init()
+    local options = { noremap = true, silent = true }
+
+    vim.keymap.set('n', '<leader>eb', '<CMD>NotionBrowser<CR>', options)
+    vim.keymap.set('n', '<leader>ee', '<CMD>NotionEdit<CR>', options)
+
     notion.setup({
         database_id = '21f1a19994538010a449f9026b0e8552',
         notion_token_cmd = { 'doppler', 'secrets', 'get', '--plain', 'NOTION_TOKEN' }
